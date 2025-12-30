b1c7513957e8f2ceead289565a5aaa4fff099679
salivala <argosnothing@gmail.com>
2025-12-29 19:30:37 -0500

    moved custom stuff out

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,31 +31,5 @@
   (dolist (file (directory-files config-dir t "\\.el$"))
     (load file nil 'nomessage)))
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval let*
-	   ((root (locate-dominating-file default-directory ".emacs"))
-	    (edir (and root (expand-file-name ".emacs" root))))
-	   (when (and edir (file-directory-p edir))
-	     (unless (boundp 'my/project-emacs-loaded)
-	       (defvar my/project-emacs-loaded
-		 (make-hash-table :test 'equal)))
-	     (unless (gethash edir my/project-emacs-loaded)
-	       (puthash edir t my/project-emacs-loaded)
-	       (dolist
-		   (f
-		    (sort
-		     (directory-files-recursively edir "\\.el\\'")
-		     #'string<))
-		 (load f nil 'nomessage))))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
