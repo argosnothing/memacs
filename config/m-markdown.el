431180ab801db4062899598efa0f8f5c1f4c1352
salivala <argosnothing@gmail.com>
2026-01-02 16:55:37 -0500

    added it to init, still nothing

--- config/m-markdown.el
+++ config/m-markdown.el
@@ -2,9 +2,10 @@
   :defer t
   :mode ("README\\.md\\'" . gfm-mode)
   :ensure t
-  :init (setq markdown-command "multimarkdown")
-  :config
+  :init
+  (setq markdown-command "multimarkdown")
   (setq markdown-fontify-code-blocks-natively t)
+  :config
   (add-to-list 'markdown-code-lang-modes '("rust" . rust-ts-mode)))
 
 (provide 'm-markdown)
