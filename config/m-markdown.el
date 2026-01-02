16b106af89d52e1e5b3b108c2013e6bba2b2bfff
salivala <argosnothing@gmail.com>
2026-01-02 16:52:01 -0500

    more things done

--- config/m-markdown.el
+++ config/m-markdown.el
@@ -2,6 +2,9 @@
   :defer t
   :mode ("README\\.md\\'" . gfm-mode)
   :ensure t
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command "multimarkdown")
+  :config
+  (setq markdown-fontify-code-blocks-natively t)
+  (add-to-list 'markdown-code-lang-modes '("rust" . rust-ts-mode)))
 
 (provide 'm-markdown)
