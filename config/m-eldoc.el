38cd4cdc4710fcf2992c29e0b59675deba6d72f0
salivala <argosnothing@gmail.com>
2026-01-02 16:33:28 -0500

    eldoc i will win eventually

--- config/m-eldoc.el
+++ config/m-eldoc.el
@@ -3,16 +3,7 @@
 (use-package eldoc-box
   :ensure t
   :demand t
-  ;; :hook
-  ;; (eglot-managed-mode . eldoc-box-hover-at-point-mode)
-  :config
-  (add-hook 'eldoc-box-buffer-hook #'eldoc-box-hover-mode t)
-  (add-hook 'eldoc-box-buffer-setup-hook #'eldoc-box-prettify-ts-errors 0 t)
-  (lambda ()
-    (when (require 'markdown-mode nil t)
-      (let ((inhibit-read-only t))
-        (delay-mode-hooks (markdown-mode))
-        (font-lock-mode 1)
-        (font-lock-ensure)))))
+  :hook
+  (eglot-managed-mode . eldoc-box-hover-at-point-mode))
 
 (provide 'm-eldoc)
