66040dd10483f7a4a987100a12822251f23d967c
salivala <argosnothing@gmail.com>
2026-01-18 16:14:20 -0500

    go away warning i hate you!

--- config/m-core.el
+++ config/m-core.el
@@ -3,6 +3,7 @@
 (define-key minibuffer-local-completion-map (kbd "C-h") #'backward-kill-word)
 (define-key minibuffer-local-must-match-map (kbd "C-h") #'backward-kill-word)
 (define-key minibuffer-local-filename-completion-map (kbd "C-h") #'backward-kill-word)
+(setq byte-compile-warnings nil)
 (electric-pair-mode 1)
 (show-paren-mode 1)
 (provide 'm-core)
